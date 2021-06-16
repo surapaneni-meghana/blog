a635c043da539e3058cdd4f760b95e57de33fe3f
surapaneni meghana <meghanasurapaneni2@gmail.com>
2021-06-16 23:26:15 +0530

    first

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,49 @@
+import "./sidebar.css"
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+          <div className="sidebarItem">
+            <span className="sidebarTitle">ABOUT ME</span>
+            <img
+              src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+              alt=""
+            />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat laboriosam voluptate beatae debitis facilis vitae, fugiat mollitia laborum delectus. Cupiditate mollitia voluptatum illo cumque, esse soluta rem excepturi saepe quas.</p>
+          </div>
+          <div className="sidebarItem">
+            <span className="sidebarTitle">CATEGORIES</span>
+              <ul className="sidebarList">
+                <li className="sidebarListItem">
+                    Life
+                </li>
+                <li className="sidebarListItem">
+                    Music
+                </li>
+                <li className="sidebarListItem">
+                    Sport
+                </li>
+                <li className="sidebarListItem">
+                    Style
+                </li>
+                <li className="sidebarListItem">
+                    Tech
+                </li>
+                <li className="sidebarListItem">
+                    Cinema
+                </li>
+              </ul>
+          </div>
+          <div className="sidebarItem">
+            <span className="sidebarTitle">FOLLOW US</span>
+            <div className="sidebarSocial">
+                <i className="sidebarIcon fab fa-facebook-square"></i>
+                <i className="sidebarIcon fab fa-instagram-square"></i>
+                <i className="sidebarIcon fab fa-pinterest-square"></i>
+                <i className="sidebarIcon fab fa-twitter-square"></i>
+            </div>
+          </div>
+        </div>
+            
+    )
+}
---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,43 @@
+import Sidebar from "../../components/sidebar/Sidebar"
+import "./settings.css"
+
+export default function Settings() {
+    return (
+      <div className="settings">
+        <div className="settingsWrapper">
+          <div className="settingsTitle">
+            <span className="settingsTitleUpdate">Update Your Account</span>
+            <span className="settingsTitleDelete">Delete Account</span>
+          </div>
+          <form className="settingsForm">
+            <label>Profile Picture</label>
+            <div className="settingsPP">
+              <img
+                src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+              />
+              <label htmlFor="fileInput">
+                <i className="settingsPPIcon far fa-user-circle"></i>{" "}
+              </label>
+              <input
+                id="fileInput"
+                type="file"
+                style={{ display: "none" }}
+                className="settingsPPInput"
+              />
+            </div>
+            <label>Username</label>
+            <input type="text" placeholder="abcde" name="name" className="settingsInput"/>
+            <label>Email</label>
+            <input type="email" placeholder="abcde@gmail.com" name="email" className="settingsInput"/>
+            <label>Password</label>
+            <input type="password" placeholder="Password" name="password" className="settingsInput"/>
+            <button className="settingsSubmitButton" type="submit">
+              Update
+            </button>
+          </form>
+        </div>
+        <Sidebar />
+      </div>
+    )
+}
